8977f8679b15d4c664d89453698ece8e8ca7bd06
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-07-12 11:52:28 +0200

    - Added sendRequest method which sends a request to the API - Added a functionality which changes the UI based on the received data from the API

--- app/src/main/java/com/example/cityweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/cityweatherapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.cityweatherapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.cityweatherapp.data.RetrofitClient
 import com.example.cityweatherapp.data.WeatherServiceInterface
@@ -12,43 +14,69 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.lang.Math.ceil
 
 class MainActivity : AppCompatActivity() {
     var weatherResponse : WeatherResponse? = null
+    var city : String? = null
+    lateinit var cityTextView : TextView
+    lateinit var weatherTextView : TextView
+    lateinit var tempTextView : TextView
+    lateinit var weatherImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        cityTextView = cityNameText
+        weatherImageView = weatherImage
+        weatherTextView = weatherText
+        tempTextView = tempText
+
+        // Caching retrieved data
+        //weatherTextView.text = weatherResponse?.weather?.get(0)?.main ?: ""
+        //tempTextView.text = (weatherResponse?.main?.temp?.minus(273)).toString()
+        //if(tempTextView.text == "null") tempTextView.text = ""
+
 
         //  Connecting with API
-        connectWithAPI()
+        val service = connectWithAPI()
+        floatingActionButton.setOnClickListener{sendRequest(service, cityNameEditText.text.toString().trim())}
+
     }
 
-    private fun connectWithAPI() {
+
+    private fun connectWithAPI() : WeatherServiceInterface {
         val retrofit = RetrofitClient.getRetrofitClient()
         val service = retrofit.create(WeatherServiceInterface::class.java)
-        service.getWeatherResponse("London", Utilities.apiKey).enqueue(
-                object : Callback<WeatherResponse>{
-                    override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
-                        Log.d("failure", t.message)
-                        Toast.makeText(this@MainActivity,
-                                "Something went wrong!", Toast.LENGTH_LONG).show()
+        return service
+    }
+
+    private fun sendRequest(service : WeatherServiceInterface, city: String) {
+        service.getWeatherResponse(city, Utilities.apiKey).enqueue(
+            object : Callback<WeatherResponse>{
+                override fun onFailure(call: Call<WeatherResponse>, t: Throwable) {
+                    Log.d("failure", t.message)
+                    Toast.makeText(this@MainActivity,
+                        "Something went wrong!", Toast.LENGTH_LONG).show()
+                }
+
+                override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
+                    if(response.isSuccessful) {
+                        this@MainActivity.weatherResponse = response.body()
+
+                        weatherTextView.text = weatherResponse?.weather?.get(0)?.main
+                        tempTextView.text = "${(weatherResponse?.main?.temp?.minus(273))?.toInt()} \u2103"
+                        cityTextView.text = city.capitalize()
                     }
-
-                    override fun onResponse(call: Call<WeatherResponse>, response: Response<WeatherResponse>) {
-                       if(response.isSuccessful) {
-                           weatherResponse = response.body()
-                           cityNameText.text = weatherResponse?.weather?.get(0)?.main ?: ""
-                       }
-                        else {
-                           Log.d("failure", response.code().toString())
-                           Toast.makeText(this@MainActivity,
-                                   "The city entered does not exist!", Toast.LENGTH_LONG).show()
-                       }
-
+                    else {
+                        Log.d("failure", response.code().toString())
+                        Toast.makeText(this@MainActivity,
+                            "The city entered does not exist!", Toast.LENGTH_LONG).show()
                     }
 
                 }
+
+            }
         )
     }
 }
--- app/src/main/java/com/example/cityweatherapp/model/WeatherResponse.kt
+++ app/src/main/java/com/example/cityweatherapp/model/WeatherResponse.kt
@@ -1,5 +1,5 @@
 package com.example.cityweatherapp.model
 
 data class WeatherResponse(var weather: List<Weather>, var main: Temperature)
-data class Weather(var main : String)
+data class Weather(var main : String, var icon: String)
 data class Temperature(var temp : Double)
